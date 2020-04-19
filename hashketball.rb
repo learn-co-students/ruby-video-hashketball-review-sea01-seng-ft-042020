14785bbd8f66ce47c82709db4c1a0359ec1ae3cf
Mathew Wheatley <mathew.p.wheatley@gmail.com>
2020-04-19 22:21:22 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -128,3 +128,132 @@ def game_hash
 end
 
 # Write code here
+def find_team(team_name)
+  # Initialize team variable for which we are searching
+  team = {}
+  # Iterate through top level hash and find player_name
+  game_hash.each_pair do |team_key, team_hash|
+    return team_hash if team_hash[:team_name] == team_name
+  end
+  # If the team was not found, return a error messages
+ "Sorry, that team name was not found, verify your input"
+end
+
+def find_player(player_name)
+  # Initialize player variable for which we are searching
+  player = {}
+  # Iterate through top level hash
+  game_hash.each_pair do |team_key, team_hash|
+    # Iterate through :players array and find player_name
+    player = team_hash[:players].find {|player_hash| player_hash[:player_name] == player_name}
+    # If a player_name was found, return the player, otherwise continue iterating
+    return player if  player
+  end
+  # If the player was not found, return a error messages
+  "Sorry, that player name was not found, verify your input"
+end
+
+def num_points_scored(player_name)
+  # Call find_player method then pull the :points scored from returned player
+  find_player(player_name)[:points]
+end
+
+def shoe_size(player_name)
+  # Call find_player method then pull the :shoe size from returned player
+  find_player(player_name)[:shoe]
+end
+
+def team_colors(team_name)
+  # Call find_team method then pull the :colors from returned player
+  find_team(team_name)[:colors]
+end
+
+def team_names
+  # Initialize team_names variable for which we are searching
+  team_names = []
+  # Iterate through top level hash
+  game_hash.each_pair do |team_key, team_hash|
+    # Collect team names into array :team_names
+    team_names.push(team_hash[:team_name])
+  end
+  # Return team_names
+  team_names
+end
+
+def player_numbers(team_name)
+  # Call find_team method then map the :players key to only pull the player :numbers
+  find_team(team_name)[:players].map {|player| player[:number]}
+end
+
+def player_stats(player_name)
+  # Call find_player method
+  find_player(player_name)
+end
+
+def big_shoe_rebounds
+  # Initialize big_shoe_player variable for which we are searching
+  big_shoe_player = []
+  # Iterate through top level hash
+  game_hash.each_pair do |team_key, team_hash|
+    # Find maximum :shoe from each team and push it to big_shoe_player
+    big_shoe_player.push(team_hash[:players].max_by {|player_hash| player_hash[:shoe]})
+  end
+  # Find gloabl maximum :shoe from big_shoe_player
+  big_shoe_player = big_shoe_player.max_by {|player_hash| player_hash[:shoe]}
+  # Return :rebounds of selected big_shoe_player
+  big_shoe_player[:rebounds]
+end
+
+def most_points_scored
+  # Initialize max_points_player variable for which we are searching
+  max_points_player = []
+  # Iterate through top level hash
+  game_hash.each_pair do |team_key, team_hash|
+    # Find maximum :points from each team and push it to max_points_player
+    max_points_player.push(team_hash[:players].max_by {|player_hash| player_hash[:points]})
+  end
+  # Find gloabl maximum :points from max_points_player
+  max_points_player = max_points_player.max_by {|player_hash| player_hash[:points]}
+  # Return :player_name of selected max_points_player
+  max_points_player[:player_name]
+end
+
+def winning_team
+  # Initialize winning_team variable for which we are searching
+  winner = {}
+  # Iterate through top level hash
+  game_hash.each_pair do |team_key, team_hash|
+    # Sum up each teams :players :points assign the result as the value of winner with their team_name as key
+    winner[team_hash[:team_name]] = team_hash[:players].sum {|player_hash| player_hash[:points]}
+  end
+  winner = winner.max_by {|team_name, points| points}
+  winner[0]
+end
+
+def player_with_longest_name
+  # Initialize long_name_player variable for which we are searching
+  long_name_player = []
+  # Iterate through top level hash
+  game_hash.each_pair do |team_key, team_hash|
+    # Find maximum :player_name.length from each team and push it to long_name_player
+    long_name_player.push(team_hash[:players].max_by {|player_hash| player_hash[:player_name].length})
+  end
+  # Find gloabl maximum :player_name]length from long_name_player
+  long_name_player = long_name_player.max_by {|player_hash| player_hash[:player_name].length}
+  # Return :player_name of selected long_name_player
+  long_name_player[:player_name]
+end
+
+def long_name_steals_a_ton?
+  # Initialize most_steals_player variable for which we are searching
+  most_steals_player =[]
+  # Iterate through top level hash
+  game_hash.each_pair do |team_key, team_hash|
+    # Find maximum :points from each team and push it to most_steals_player
+    most_steals_player.push(team_hash[:players].max_by {|player_hash| player_hash[:steals]})
+  end
+  # Find gloabl maximum :points from most_steals_player
+  most_steals_player = most_steals_player.max_by {|player_hash| player_hash[:steals]}
+  # Compare player_name of selected most_steals_player against that returned by method player_with_longest_name
+  most_steals_player[:player_name] == player_with_longest_name
+end
